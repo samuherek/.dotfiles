b13d93892841b3cff2b603441c674c11421a739a
Sam Uherek <samuherekbiz@gmail.com>
2023-09-12 10:47:19 +0200

    macbook 16 changes"

--- nvim/.config/nvim/lua/samuherek/init.lua
+++ nvim/.config/nvim/lua/samuherek/init.lua
@@ -1,3 +1,8 @@
-require("samuherek.remap")
-require("samuherek.packer")
-require("samuherek.set")
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+    require("samuherek.remap")
+    require("samuherek.packer")
+    require("samuherek.set")
+end
