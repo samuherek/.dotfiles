5ee8337318f8d71446ef1d281603e2d4adbdd2e2
Sam Uherek <samuherekbiz@gmail.com>
2023-10-26 15:10:02 +0200

    move from packer to lazy

---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,55 @@
+return {
+    -- Telescope plugin
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    -- Rose pine color theme
+    {
+        'rose-pine/neovim',
+        -- as = 'rose-pine',
+        -- variant = 'moon',
+        -- dark_variant = 'moon',
+        -- disable_italics = true,
+        -- groups = {
+        --   comment = 'muted',
+        --}
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = function()
+            pcall(require('nvim-treesitter.install').update {with_sync = true})
+        end,
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        }
+    },
+    'nvim-treesitter/playground',
+    'ThePrimeagen/harpoon',
+    'mbbill/undotree',
+    'tpope/vim-fugitive',
+    'sbdchd/neoformat',
+    'prettier/vim-prettier',
+    'jparise/vim-graphql',
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
+}
+    }
--- nvim/.config/nvim/lua/samuherek/init.lua
+++ nvim/.config/nvim/lua/samuherek/init.lua
@@ -3,9 +3,38 @@ if vim.g.vscode then
     vim.keymap.set("n", "<C-d>", "<C-d>zz")
     vim.keymap.set("n", "<C-u>", "<C-u>zz")
 else
-    -- ordinary Neovim
-    require("samuherek.remap")
-    require("samuherek.packer")
+
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath,
+        })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
     require("samuherek.set")
-    require("samuherek.centered_vim")
+
+    require('lazy').setup('plugins')
+
+    vim.cmd('colorscheme rose-pine')
+
+    require("samuherek.remap")
+    --require("samuherek.centered_vim")
+-- harpoon plugin
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end)
+
 end
