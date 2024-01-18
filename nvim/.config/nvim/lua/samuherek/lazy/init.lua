46b28e21cbaf7f88137ac991bb2ef0cc80d81905
Sam Uherek <samuherekbiz@gmail.com>
2024-01-18 09:23:39 +0100

    added nvim comment plugin

--- nvim/.config/nvim/lua/samuherek/lazy/init.lua
+++ nvim/.config/nvim/lua/samuherek/lazy/init.lua
@@ -1,8 +1,18 @@
 return {
     'nvim-lua/plenary.nvim',
     'tpope/vim-fugitive',
-    'github/copilot.nvim',
+    -- 'github/copilot.nvim',
     'folke/zen-mode.nvim',
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        },
+        lazy = false,
+        config = function()
+            require('Comment').setup()
+        end
+    }
     -- 'sbdchd/neoformat',
     -- 'jparise/vim-graphql',
 }
