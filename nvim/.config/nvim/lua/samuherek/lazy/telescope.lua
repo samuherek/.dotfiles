009f7160f37824e9bbaca3f46146cacd39358679
Sam Uherek <samuherekbiz@gmail.com>
2024-09-26 13:46:37 +0200

    update telescope shortcuts

--- nvim/.config/nvim/lua/samuherek/lazy/telescope.lua
+++ nvim/.config/nvim/lua/samuherek/lazy/telescope.lua
@@ -8,9 +8,11 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>gs', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
+        vim.keymap.set('n', '<leader>gg', builtin.grep_string, {})
+        vim.keymap.set("n", "<leader>gb", builtin.buffers, {})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
