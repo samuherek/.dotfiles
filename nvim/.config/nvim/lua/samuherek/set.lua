5ecd61cc024f93d0ffd7458c83cdc5adc8bb0bfb
Sam Uherek <samuherekbiz@gmail.com>
2025-01-25 12:17:18 +0100

    finally enable the case insenstive search

--- nvim/.config/nvim/lua/samuherek/set.lua
+++ nvim/.config/nvim/lua/samuherek/set.lua
@@ -17,6 +17,9 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
