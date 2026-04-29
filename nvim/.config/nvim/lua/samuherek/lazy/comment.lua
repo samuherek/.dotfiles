095b519d7a5892fcf2a1fcc6fe70b4a52d92f0c4
Sam Uherek <samuherekbiz@gmail.com>
2026-04-29 15:59:21 +0200

    add comments for the templ files

--- nvim/.config/nvim/lua/samuherek/lazy/comment.lua
+++ nvim/.config/nvim/lua/samuherek/lazy/comment.lua
@@ -1,4 +1,20 @@
 return {
 	"numToStr/Comment.nvim",
-	lazy = true,
+	event = "VeryLazy",
+	config = function()
+		-- Not sure if this should/can live here or not.
+		-- but it works so let's keep it for now
+		vim.filetype.add({
+			extension = {
+				templ = "templ",
+			},
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "templ",
+			callback = function()
+				vim.bo.commentstring = "// %s"
+			end,
+		})
+	end,
 }
