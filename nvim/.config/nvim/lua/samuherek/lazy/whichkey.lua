c868f1383025dd829e482a6df94d0220548c2268
Sam Uherek <samuherekbiz@gmail.com>
2026-05-20 13:53:33 +0200

    small helper descriptions for the whichkey

--- nvim/.config/nvim/lua/samuherek/lazy/whichkey.lua
+++ nvim/.config/nvim/lua/samuherek/lazy/whichkey.lua
@@ -1,6 +1,14 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
+	config = function()
+		local wk = require("which-key")
+
+		wk.add({ { "<leader>t", group = "Telescope..." } })
+		wk.add({ { "<leader>q", group = "Quickfix..." } })
+		wk.add({ { "<leader>a", group = "AI..." } })
+		wk.add({ { "<leader>w", group = "Window..." } })
+	end,
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
@@ -8,6 +16,13 @@ return {
 		delay = function(ctx)
 			return ctx.plugin and 0 or 1000
 		end,
+		icons = {
+			mappings = false, -- disable the font icons
+			breadcrumb = "»",
+			separator = "➜",
+			group = "+",
+			ellipsis = "…",
+		},
 	},
 	keys = {
 		{
