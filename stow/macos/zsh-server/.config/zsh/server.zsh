7fe7a9070b1e795da23a76230c7a14c5e9a0659c
Sam Uherek <samuherekbiz@gmail.com>
2026-08-11 19:52:46 +0200

    ssh connection indicator

--- stow/macos/zsh-server/.config/zsh/server.zsh
+++ stow/macos/zsh-server/.config/zsh/server.zsh
@@ -3,6 +3,10 @@ VIM="nvim"
 # Let's set colored prompt
 PS1='%F{blue}%n@%m%f %F{cyan}%1~%f %# '
 
+if [[ -n "$SSH_CONNECTION" ]]; then
+    PS1='%F{yellow}󰢹 SSH%f '"$PS1"
+fi
+
 export EDITOR=$VIM
 export GIT_EDITOR=$VIM
 
